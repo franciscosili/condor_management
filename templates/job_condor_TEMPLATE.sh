9f6352b6daf66fd5134506f398b357d789a9c290
Francisco Sili <francisco.sili@cern.ch>
2023-12-01 11:35:27 +0100

    add templates

---
+++ templates/job_condor_TEMPLATE.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+check_command_success() {
+    echo "$@"
+    "$@"
+    if [ $? -eq 0 ]; then
+        echo "Command successful"
+    else
+        echo "Command failed"
+        exit 1
+    fi
+}
+
+
+echo $PWD
+
+mkdir code
+cd code
+
+# create tarbal
+check_command_success CMD_COPY
+
+echo $PWD
+
+cd "$(dirname "$0")"
+
+echo $PWD
+
+ls -la
+#pwd
+check_command_success source setup.sh
+
+# command to execute
+check_command_success CMD
+
+# delete files that where copied from the code directory
+check_command_success DELETEFILES
